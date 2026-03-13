d566ce8e85631051113b2dff3fc9f72bd1f207c4
ENKI <amit.khanna.1082@gmail.com>
2026-03-13 07:55:45 +0700

    Create shipping.js

---
+++ shipping.js
@@ -0,0 +1,39 @@
+/* SHIPPING / CONTAINER COST DATABASE */
+
+const containerShipping = {
+
+LCL:{
+
+name:"Small Shipment (LCL)",
+cost:300
+
+},
+
+C20:{
+
+name:"20ft Container",
+cost:1800
+
+},
+
+C40:{
+
+name:"40ft Container",
+cost:3200
+
+}
+
+};
+
+
+/* OPTIONAL: CONTAINER CAPACITY DATABASE */
+
+const containerCapacity = {
+
+LCL:1000,
+
+C20:10000,
+
+C40:20000
+
+};
