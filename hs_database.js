969c70f1dfff4313771a413378be14d58195702a
ENKI <amit.khanna.1082@gmail.com>
2026-03-13 07:47:42 +0700

    Add HS code product database with duties

---
+++ hs_database.js
@@ -0,0 +1,75 @@
+/* HS CODE PRODUCT DATABASE */
+
+const hsDatabase = [
+
+{
+hs:"6109",
+name:"T-Shirt Cotton",
+thailandDuty:10,
+bangladeshDuty:25
+},
+
+{
+hs:"3304",
+name:"Cosmetics / Face Cream",
+thailandDuty:20,
+bangladeshDuty:25
+},
+
+{
+hs:"3305",
+name:"Hair Color",
+thailandDuty:20,
+bangladeshDuty:25
+},
+
+{
+hs:"3924",
+name:"Plastic Household Goods",
+thailandDuty:15,
+bangladeshDuty:25
+},
+
+{
+hs:"9615",
+name:"Hair Accessories",
+thailandDuty:15,
+bangladeshDuty:25
+},
+
+{
+hs:"4202",
+name:"Handbags and Travel Bags",
+thailandDuty:10,
+bangladeshDuty:25
+},
+
+{
+hs:"6403",
+name:"Leather Shoes",
+thailandDuty:10,
+bangladeshDuty:25
+},
+
+{
+hs:"8517",
+name:"Mobile Accessories",
+thailandDuty:5,
+bangladeshDuty:10
+},
+
+{
+hs:"9403",
+name:"Furniture",
+thailandDuty:15,
+bangladeshDuty:25
+},
+
+{
+hs:"9503",
+name:"Toys",
+thailandDuty:10,
+bangladeshDuty:25
+}
+
+];
